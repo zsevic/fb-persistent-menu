4cfa3c9597d657db471f25ab021a0b7d0fdeebb0
zsevic <zsevic95@gmail.com>
2017-12-04 02:13:13 +0100

    :tada: add main functionalities

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 
-const argv = require('yargs').argv;
-const lib = require('../lib');
-const accessToken = process.env.access_token;
+const argv = require('yargs').argv
+const lib = require('../lib')
 
-if(argv.json){
-    try{
-        let data=JSON.parse(argv.json);
-        lib.addPersistentMenu(accessToken,data);
-    }catch(e){
-        console.log(`JSON is not well formatted!`);
+if (argv.token) {
+  if (argv.settings) {
+    try {
+      let data = JSON.parse(argv.settings)
+      lib.addPersistentMenu(argv.token, data)
+    } catch (e) {
+      lib.handleError(`JSON is not well formatted`)
     }
-}else{
-    console.log(`There are no settings`);
+  } else if (argv.remove || argv.r) {
+    lib.removePersistentMenu(argv.token)
+  } else {
+    lib.usage()
+  }
+} else {
+  lib.usage()
 }
